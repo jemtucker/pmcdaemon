3d6cf5b4a47da07a5798cf4c50c14fa10b081fe7
JemTucker <jem.tucker@gmail.com>
2015-04-03 08:41:08 +0100

    Initial Commit

---
+++ PiPlayer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PiPlayer
+//
+//  Created by Jem Tucker on 03/04/2015.
+//  Copyright (c) 2015 Jem Tucker. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
