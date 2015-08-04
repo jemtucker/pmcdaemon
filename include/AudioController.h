bb04db7dae9026494260e82a7dea6c6d96727e07
JemTucker <jem.tucker@googlemail.com>
2015-08-04 22:40:20 +0100

    Change design and started AudioController

---
+++ include/AudioController.h
@@ -0,0 +1,38 @@
+//
+//  AudioController.h
+//  pmcdaemon
+//
+//  Created by Jem Tucker on 04/08/2015.
+//  Copyright (c) 2015 Jem Tucker. All rights reserved.
+//
+
+#ifndef __pmcdaemon__AudioController__
+#define __pmcdaemon__AudioController__
+
+#include <stdio.h>
+
+#include <ao/ao.h>
+#include <mpg123.h>
+#include <mutex>
+
+enum AudioControllerState {
+    PLAYING = 1,
+    STOPPED = 2
+};
+
+class AudioController {
+    std::mutex stateMutex;
+    
+    mpg123_handle *mh;
+    ao_device *ao;
+    
+    AudioControllerState state = STOPPED;
+    
+    size_t playStream(void *buffer, size_t size, size_t nmemb, void *userp);
+public:
+    AudioControllerState currentState();
+    void setCurrentState(AudioControllerState s);
+    static size_t staticPlayStream(void *buffer, size_t size, size_t nmemb, void *userp);
+};
+
+#endif /* defined(__pmcdaemon__AudioController__) */
---
+++ src/AudioController.cpp
@@ -0,0 +1,66 @@
+//
+//  AudioController.cpp
+//  pmcdaemon
+//
+//  Created by Jem Tucker on 04/08/2015.
+//  Copyright (c) 2015 Jem Tucker. All rights reserved.
+//
+
+#include "AudioController.h"
+
+#include <curl/curl.h>
+
+
+
+size_t AudioController::playStream(void *buffer, size_t size, size_t nmemb, void *userp) {
+    if (static_cast<AudioController *>(userp)->currentState() == STOPPED)
+        return CURL_READFUNC_ABORT;
+    
+    int err;
+    off_t frame_offset;
+    unsigned char *audio;
+    size_t done;
+    ao_sample_format format;
+    int channels, encoding;
+    long rate;
+    
+    mpg123_feed(mh, (const unsigned char*) buffer, size * nmemb);
+    do {
+        err = mpg123_decode_frame(mh, &frame_offset, &audio, &done);
+        switch(err) {
+            case MPG123_NEW_FORMAT:
+                mpg123_getformat(mh, &rate, &channels, &encoding);
+                format.bits = mpg123_encsize(encoding) * 8;  // Bits to encode
+                format.rate = (int) rate;
+                format.channels = channels;
+                format.byte_format = AO_FMT_NATIVE;
+                format.matrix = 0;
+                ao = ao_open_live(ao_default_driver_id(), &format, NULL);
+                break;
+            case MPG123_OK:
+                ao_play(ao, (char *) audio, (uint_32) done);
+                break;
+            case MPG123_NEED_MORE:
+                break;
+            default:
+                break;
+        }
+    } while(done > 0);
+    
+    return size * nmemb;
+}
+
+size_t AudioController::staticPlayStream(void *buffer, size_t size, size_t nmemb, void *userp) {
+    return static_cast<AudioController*>(userp)->playStream(buffer, size, nmemb, userp);
+}
+
+void AudioController::setCurrentState(AudioControllerState s) {
+    std::lock_guard<std::mutex> lock(stateMutex);
+    state = s;
+}
+
+AudioControllerState AudioController::currentState() {
+    std::lock_guard<std::mutex> lock(stateMutex);
+    return state;
+}
+
